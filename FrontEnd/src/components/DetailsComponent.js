dc4796b9abf3c55403fbadd708e312fda7eb882e
dev-smurillo <smurillo.dev@gmail.com>
2024-06-05 15:53:57 -0600

    new edit

--- FrontEnd/src/components/DetailsComponent.js
+++ FrontEnd/src/components/DetailsComponent.js
@@ -4,21 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Cookies from 'js-cookie'
 import axios from 'axios'
 import FileDisplay from './FileDisplay'
+
+
+
 export default function DetailsPage({ title, eventId }) {
   const [formData, setFormData] = useState({
     name: "",
     description: "",
     type: "",
     status: "",
-    startDate: ""
+    startDate: "",
+    taskList: []
   });
 
-  const [event, setEvent] = useState({})
   const [isLoading, setIsLoading] = useState(true);
   const [formSuccess, setFormSuccess] = useState(false);
   const [formSuccessMessage, setFormSuccessMessage] = useState("");
-  const [files, setFiles] = useState([])
-  const [tasks, setTasks] = useState([])
+  const [files, setFiles] = useState([]);
+  const [editMode, setEditMode] = useState(false); // State variable for edit mode
+
+  const [newTask, setNewTask] = useState(""); // State variable for new task input
 
   useEffect(() => {
     const token = Cookies.get('plan_ahead_user_token');
@@ -29,32 +34,25 @@ export default function DetailsPage({ title, eventId }) {
         }
       })
         .then(res => {
-          if (res.status == 200) {
-
-            setFiles(res.data.files)
-            setTasks(res.data.tasks)
+          if (res.status === 200) {
+            setFiles(res.data.files);
             setFormData({
               name: res.data.title,
               description: res.data.description,
               type: res.data.type,
               status: res.data.completed,
-              startDate: new Date(res.data.startDate).toISOString().split('T')[0]
+              startDate: new Date(res.data.startDate).toISOString().split('T')[0],
+              taskList: res.data.tasks.map(task => task.name)
             });
             setIsLoading(false);
-
           }
         })
         .catch(err => {
-          console.log('Algo ocurrió');
+          console.log('Something went wrong');
           setIsLoading(false);
         });
     }
   }, [eventId]);
-
-  if (isLoading) {
-    return <p>Cargando...</p>
-  }
-
 
   const handleInput = (e) => {
     const fieldName = e.target.name;
@@ -66,8 +64,32 @@ export default function DetailsPage({ title, eventId }) {
     }));
   };
 
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
+
+  const handleTaskInputChange = (e, index) => {
+    const newTaskList = [...formData.taskList];
+    newTaskList[index] = e.target.value;
+    setFormData(prevState => ({
+      ...prevState,
+      taskList: newTaskList
+    }));
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setFormData(prevState => ({
+        ...prevState,
+        taskList: [...prevState.taskList, newTask]
+      }));
+      setNewTask(""); // Clear the new task input field
+    }
+  };
+
   const submitForm = async (e) => {
     e.preventDefault();
+    if (!editMode) return; // Disable form submission if not in edit mode
 
     const formURL = e.target.action;
     const data = new FormData();
@@ -92,6 +114,8 @@ export default function DetailsPage({ title, eventId }) {
           description: "",
           type: "",
           status: "",
+          startDate: "",
+          taskList: []
         });
         setFormSuccess(true);
         setFormSuccessMessage(responseData.submission_text);
@@ -111,10 +135,8 @@ export default function DetailsPage({ title, eventId }) {
             <h1>Event details</h1>
           </div>
           <div className='col-md-6 col-sm-12'>
-            <button type="button" className="btn btn-secondary">
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-                <path d="M4.5 13.793l-1.5 1.5v.707h.707l1.5-1.5-.707-.707zm9-9l-11 11-3 1 1-3 11-11 3-1-1 3zm-10.586 9.378l9-9 .707.707-9 9-.707-.707z" />
-              </svg>
+            <button type="button" className="btn btn-secondary" onClick={toggleEditMode}>
+              {editMode ? "Cancel Edit" : "Edit"} {/* Toggle button text based on edit mode */}
             </button>
           </div>
           {formSuccess ? (
@@ -128,7 +150,15 @@ export default function DetailsPage({ title, eventId }) {
                 <div className='col-6'>
                   <div className="form-group mt-1">
                     <label htmlFor="name">Title</label>
-                    <input type="text" className="form-control" name="name" value={formData.name} placeholder="Enter Title" onChange={handleInput} />
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      value={formData.name}
+                      placeholder="Enter Title"
+                      onChange={handleInput}
+                      disabled={!editMode} // Disable input based on edit mode
+                    />
                   </div>
                   <div className="form-group mt-1">
                     <label htmlFor="description">Description</label>
@@ -140,71 +170,110 @@ export default function DetailsPage({ title, eventId }) {
                       onChange={handleInput}
                       rows={4}
                       spellCheck={false}
+                      disabled={!editMode} // Disable textarea based on edit mode
                     />
                   </div>
-
                   <div className="form-group mt-1">
-                    <label htmlFor="type">Tipo de evento</label>
-                    <input type="text" className="form-control" name="type" value={formData.type} placeholder="Event type" onChange={handleInput} />
+                    <label htmlFor="type">Type of event</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="type"
+                      value={formData.type}
+                      placeholder="Event type"
+                      onChange={handleInput}
+                      disabled={!editMode} // Disable input based on edit mode
+                    />
                   </div>
                   <div className="form-group mt-1">
-                    <label htmlFor="createdAt">Created At</label>
+                    <label htmlFor="startDate">Start Date</label>
                     <input
                       type="date"
                       className="form-control"
                       name="startDate"
                       value={formData.startDate}
                       onChange={handleInput}
+                      disabled={!editMode} // Disable input based on edit mode
                     />
                   </div>
-
                 </div>
                 <div className='col-6'>
                   <div className="form-group mt-1">
                     <label htmlFor="status">Status</label>
-                    <select className="form-control" name="status" value={formData.status} onChange={handleInput}>
-                      {
-                        formData.status == true ? <option value="complete" defaultChecked="true" >Complete</option> : <option value="incomplete">Incomplete</option>
-                      }
-
-                      {
-                        formData.status == false ? <option value="complete" >Complete</option> : <option value="incomplete" defaultChecked="true" >Incomplete</option>
-                      }
-
+                    <select
+                      className="form-control"
+                      name="status"
+                      value={formData.status}
+                      onChange={handleInput}
+                      disabled={!editMode} // Disable select based on edit mode
+                    >
+                      <option value="complete">Complete</option>
+                      <option value="incomplete">Incomplete</option>
                     </select>
                   </div>
+                </div>
+              </div>
+              <hr className='mt-4'></hr>
+              <div className='row'>
+                <div className='col-sm-12 col-md-6'>
                   <div className="form-group mt-1 h-75">
                     <label htmlFor="taskList">Task List</label>
-                    {
-                      tasks.length !== 0 ?
-                        tasks.map((task, index) => (
-                          <div key={index}>
-                            {task.name}
-                          </div>
-                        )) :
-                        <p>No tasks for this event</p>
-                    }
-
+                    <table className="table">
+                      <tbody>
+                        {formData.taskList.map((task, index) => (
+                          <tr key={index}>
+                            <td>
+                              <input
+                                type="text"
+                                className="form-control"
+                                value={task}
+                                onChange={(e) => handleTaskInputChange(e, index)}
+                                disabled={!editMode}
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                        <tr>
+                          <td>
+                            <div className="input-group">
+                              <input
+                                type="text"
+                                className="form-control"
+                                value={newTask}
+                                onChange={(e) => setNewTask(e.target.value)}
+                                disabled={!editMode}
+                              />
+                              <button
+                                type="button"
+                                className="btn btn-primary"
+                                onClick={handleAddTask}
+                                disabled={!editMode}
+                              >
+                                Add Task
+                              </button>
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
-                <div className='row mt-3'>
+                <div className='col-sm-12 col-md-6'>
                   <div className="form-group mt-1">
-                    <label htmlFor="relatedFiles">Related Files</label>
-                    {
-                      files.length !== 0 ?
-                        files.map((file, index) => (
-                          <FileDisplay key={index} url={file.url} nombre={file.filename} />
-                        )) :
-                        <p>No files for this event</p>
-                    }
-
+                    <label htmlFor="fileList">Related Files</label>
+                    {files.length !== 0 ? (
+                      files.map((file, index) => (
+                        <FileDisplay key={index} url={file.url} nombre={file.filename} />
+                      ))
+                    ) : (
+                      <p>No files for this event</p>
+                    )}
                   </div>
-
                 </div>
-
-
               </div>
-              <button type="submit" className="btn btn-primary mt-5">Save</button>
+              <button type="submit" className="btn btn-primary mt-5" disabled={!editMode}>
+                Save {/* Enable Save button only in edit mode */}
+              </button>
             </form>
           )}
         </div>
